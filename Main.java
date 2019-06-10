b7a550809e6f1259d3ccc025e942978252eb5a6f
Noulsy <nicolas.goolen@gmail.com>
2019-06-11 00:31:52 +0200

    ajout de commentaire dans le Main depuis github

--- Main.java
+++ Main.java
@@ -3,6 +3,9 @@ package com.nico;
 public class Main {
 
     public static void main(String[] args) {
+	/**
+     * Nouveau commentaire
+     */
 	System.out.println("Bijour bijour M Vincent");
     }
 }
